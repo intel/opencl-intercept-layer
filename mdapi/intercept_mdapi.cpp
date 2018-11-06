08021cd75a0c7307b081017bd9913f697882fc47
Ben Ashbaugh <ben.ashbaugh@intel.com>
2018-11-05 21:11:27 -0800

    minor fix for creating MDAPI command queues with properties

--- mdapi/intercept_mdapi.cpp
+++ mdapi/intercept_mdapi.cpp
@@ -44,10 +44,9 @@ static bool convertPropertiesToOCL1_2(
                 switch( properties[ i + 1 ] )
                 {
                 case CL_QUEUE_PROFILING_ENABLE:
-                    ocl1_2_properties |= CL_QUEUE_PROFILING_ENABLE;
-                    break;
                 case CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE:
-                    ocl1_2_properties |= CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE;
+                case CL_QUEUE_PROFILING_ENABLE | CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE:
+                    ocl1_2_properties |= properties[ i + 1 ];
                     break;
                 default:
                     return false;
