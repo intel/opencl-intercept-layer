4d54a541ddd2fa8a5607662d48f2da82bc0e0ed0
Ben Ashbaugh <ben.ashbaugh@intel.com>
2022-09-10 09:40:44 -0700

    add a control to demangle kernel names (#274)
    * add a control to demangle kernel names
    
    * remove unneeded typeinfo name for

---
+++ intercept/src/demangle.h
@@ -0,0 +1,37 @@
+/*
+// Copyright (c) 2022 Intel Corporation
+//
+// SPDX-License-Identifier: MIT
+*/
+
+#pragma once
+
+#if defined(USE_DEMANGLE)
+#include <cxxabi.h>
+#endif
+#include <string>
+
+static inline std::string demangle(const std::string& in)
+{
+#if defined(USE_DEMANGLE)
+    std::string ret;
+    int status = 0;
+    char* demangled = abi::__cxa_demangle(in.c_str(), NULL, NULL, &status);
+    if( status != 0 )
+    {
+        ret = in;
+    }
+    else
+    {
+        const std::string skip("typeinfo name for ");
+        ret = demangled;
+        if (ret.rfind(skip, 0) == 0) {
+            ret.erase(0, skip.length());
+        }
+    }
+    free(demangled);
+    return ret;
+#else
+    return in;
+#endif
+}
