1cb0a20747e9024265819eb88bd79392c5d146af
Nigel Overmars <nigelovermars@outlook.com>
2023-02-27 20:02:13 +0100

    Update validate script
    Fixed typo (Bump->Dump)
    Now works on Windows and Linux

--- scripts/capture_and_validate.py
+++ scripts/capture_and_validate.py
@@ -14,46 +14,48 @@ import numpy as np
 parser = argparse.ArgumentParser(description='Helper script to extract and validate single kernel from OpenCL app.')
 parser.add_argument('-enqueue_number', '--num', type=int, dest='enqueue_number',
                     help='The enqueue number that should be captured ')
-parser.add_argument('-cli', type=ascii, dest='cli_location',
+parser.add_argument('-cli', dest='cli_location',
                     help='Location of the OpenCL-Intercept-Layer')
-parser.add_argument('-program', '--p', type=ascii, dest='app_location',
+parser.add_argument('-program', '--p', dest='app_location',
                     help='Location of the app from which should be captured')
-parser.add_argument('-args', '--a', nargs='+', default=[], dest='args')
+parser.add_argument('-args', '--a', nargs=argparse.REMAINDER, default=[], dest='args')
 
 args = parser.parse_args()
-
-app_name = args.app_location[args.app_location.rfind('/') + 1:-1]
 
 intercept_location_win = "C:\Intel\CLIntercept_Dump\\"
 intercept_location_posix = "~/CLIntercept_Dump/"
 intercept_location = ""
+delim = ""
 
 if os.name == 'nt':
     intercept_location = intercept_location_win
+    delim = "\\"
 elif os.name == 'posix':
     intercept_location = intercept_location_posix
+    delim = "/"
 else:
     print("Unknown platform, exiting!")
     exit()
 
+app_name = args.app_location[args.app_location.rfind(delim) + 1:]
+
 os.environ['CLI_DumpReplayKernelEnqueue'] = str(args.enqueue_number)
 os.environ['CLI_DumpBuffersAfterEnqueue'] = str(1)
-os.environ['CLI_BumpBuffersMinEnqueue'] = str(args.enqueue_number)
-os.environ['CLI_BumpBuffersMaxEnqueue'] = str(args.enqueue_number)
+os.environ['CLI_DumpBuffersMinEnqueue'] = str(args.enqueue_number)
+os.environ['CLI_DumpBuffersMaxEnqueue'] = str(args.enqueue_number)
 os.environ['CLI_InitializeBuffers'] = str(1)
 
-# Build command to be executed
-command = args.cli_location + " " + args.app_location + " " + ' '.join(args.args)
-
 # Run ./cliloader with CLI_DumpReplayKernelEnqueue=${EnqueueNumber}
-os.system(command)
+command = [args.cli_location, args.app_location]
+command.extend(args.args)
+subprocess.run(command)
 
 replay_location = os.path.join(intercept_location, app_name, "Replay", "Enqueue_" + str(args.enqueue_number), "")
 replay_location = os.path.expanduser(replay_location)
 
 # Run extracted kernel to dump output buffers
 os.chdir(replay_location)
-os.system("python run.py")
+subprocess.run(["python", "run.py"])
 
 print("\nNow starting validation!")
 
@@ -78,19 +80,21 @@ else:
 dumped_buffers = gl.glob("./Enqueue_" + padded_enqueue_num + "_Kernel_*.bin")
 dumped_hashes = {}
 for dumped_buffer in dumped_buffers:
-    idx = int(re.findall(r'\d+', dumped_buffer)[1])
+    start = dumped_buffer.find("_Arg_")
+    idx = int(re.findall(r'\d+', dumped_buffer[start:])[0])
     dumped_hashes[idx] = hashlib.md5(np.fromfile(dumped_buffer)).hexdigest()
 
-all_equal = True
-for pos in replayed_hashes.keys():
-    if replayed_hashes[pos] == dumped_hashes[pos]:
-        print(f"Pos: {pos} is equal")
-    else:
-        print(f"Pos: {pos} is not equal")
-        all_equal = False
-
-if all_equal:
-    print("Replayed standalone kernel produces correct results")
+all_equal = False
+if len(replayed_hashes) == len(dumped_hashes):
+    for pos in replayed_hashes.keys():
+        if replayed_hashes[pos] == dumped_hashes[pos]:
+            all_equal = True
+            print(f"Pos: {pos} is equal")
+        else:
+            print(f"Pos: {pos} is not equal")
+            all_equal = False
 else:
     print("Replayed standalone kernel differs from app's result, " 
            "please open an issue on the Github so that we can look into it!")
+if all_equal:
+    print("Replayed standalone kernel produces correct results")
