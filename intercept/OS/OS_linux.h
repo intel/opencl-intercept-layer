917d8908b9133961ef93146964323b1b1b898913
Ben Ashbaugh <ben.ashbaugh@intel.com>
2025-10-29 15:33:11 -0700

    clean up and modernize the OS services files

--- intercept/OS/OS_linux.h
+++ intercept/OS/OS_linux.h
@@ -6,7 +6,37 @@
 
 #pragma once
 
-#include "OS_linux_common.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dlfcn.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <string.h>
+#include <syslog.h>
+#include <unistd.h>
+
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#ifdef __ANDROID__
+#include <android/log.h>
+#endif
+
+#ifdef __FreeBSD__
+#include <pthread_np.h>
+#include <sys/user.h>
+#include <libutil.h>
+#endif
+
+#if defined(__linux__)
+// Workaround for older glibc versions that do not have gettid:
+#if __GLIBC__ == 2 && __GLIBC_MINOR__ < 30
+    #include <sys/syscall.h>
+    #define gettid() syscall(SYS_gettid)
+#endif
+#endif
 
 // Since we just need the symbol clGetPlatformIDs, we can use any OpenCL
 // version.
@@ -18,13 +48,51 @@
 namespace OS
 {
 
-class Services : public Services_Common
+class Services
 {
 public:
+    static const char* ENV_PREFIX;
+    static const char* CONFIG_FILE;
+    static const char* SYSTEM_DIR;
+    static const char* LOG_DIR;
+    static bool        APPEND_PID;
+
     Services( void* pGlobalData );
-    ~Services();
+    Services( const Services& ) = delete;
+    Services& operator=( const Services& ) = delete;
 
     bool    Init();
+
+    uint64_t    GetProcessID() const;
+    uint64_t    GetThreadID() const;
+
+    std::string GetProcessName() const;
+
+    bool    GetControl(
+                const std::string& name,
+                void* pValue,
+                size_t size ) const;
+
+    void    OutputDebugString(
+                const std::string& str ) const;
+
+    void*   LoadLibrary(
+                const std::string& libraryName ) const;
+    void    UnloadLibrary(
+                void*& pLibrary ) const;
+
+    void*   GetFunctionPointer(
+                void* pLibrary,
+                const std::string& functionName ) const;
+
+    void    GetDumpDirectoryName(
+                const std::string& subDir,
+                std::string& directoryName ) const;
+    void    GetDumpDirectoryNameWithoutPid(
+                const std::string& subDir,
+                std::string& directoryName ) const;
+    void    MakeDumpDirectories(
+                const std::string& fileName ) const;
 
     bool    GetCLInterceptName(
                 std::string& name ) const;
@@ -53,12 +121,181 @@ public:
                 std::string& str ) const;
 
 private:
-    DISALLOW_COPY_AND_ASSIGN( Services );
+    bool    GetControlFromFile(
+                const std::string& fileName,
+                const std::string& controlName,
+                void* pValue,
+                size_t size ) const;
 };
 
 inline bool Services::Init()
 {
-    return Services_Common::Init();
+    return true;
+}
+
+inline uint64_t Services::GetProcessID() const
+{
+    return getpid();
+}
+
+inline uint64_t Services::GetThreadID() const
+{
+#if defined(__linux__)
+    return gettid();
+#elif defined(__FreeBSD__)
+    return pthread_getthreadid_np();
+#else
+#pragma message("Not sure how to implement GetThreadID()")
+    return 0;
+#endif
+}
+
+inline std::string Services::GetProcessName() const
+{
+    char    processName[ 1024 ];
+    char*   pProcessName = processName;
+
+#if defined(__linux__)
+    size_t  bytes = readlink(
+        "/proc/self/exe",
+        processName,
+        sizeof( processName ) - 1 );
+    if( bytes )
+    {
+        processName[ bytes ] = '\0';
+
+        pProcessName = strrchr( processName, '/' );
+        pProcessName++;
+    }
+#elif defined(__FreeBSD__)
+    struct kinfo_proc* proc = kinfo_getproc(getpid());
+    if( proc )
+    {
+        strncpy( processName, proc->ki_comm, sizeof( processName ) );
+        processName[ sizeof( processName ) - 1 ] = '\0';
+
+        free(proc);
+    }
+#else
+#pragma message("Not sure how to implement GetProcessName()")
+    if (false) {}
+#endif
+    else
+    {
+        strncpy( processName, "process.exe", sizeof( processName ) );
+        processName[ sizeof( processName ) - 1 ] = 0;
+    }
+
+    return std::string(pProcessName);
+}
+
+inline void Services::OutputDebugString(
+    const std::string& str ) const
+{
+    syslog( LOG_USER | LOG_INFO, "%s", str.c_str() );
+}
+
+inline void* Services::LoadLibrary(
+    const std::string& libraryName ) const
+{
+    void* pLibrary = dlopen( libraryName.c_str(), RTLD_NOW | RTLD_GLOBAL );
+    //if( pLibrary == NULL )
+    //{
+    //    fprintf(stderr, "dlopen() error: %s\n", dlerror());
+    //}
+    return pLibrary;
+}
+
+inline void Services::UnloadLibrary(
+    void*& pLibrary ) const
+{
+    dlclose( pLibrary );
+    pLibrary = NULL;
+}
+
+inline void* Services::GetFunctionPointer(
+    void* pLibrary,
+    const std::string& functionName ) const
+{
+    if( pLibrary )
+    {
+        return dlsym( pLibrary, functionName.c_str() );
+    }
+    else
+    {
+        return dlsym( RTLD_NEXT, functionName.c_str() );
+    }
+}
+
+inline void Services::GetDumpDirectoryNameWithoutPid(
+    const std::string& subDir,
+    std::string& directoryName ) const
+{
+    if( LOG_DIR )
+    {
+        // Return log dir override if set in regkeys
+        directoryName = LOG_DIR;
+    }
+    else
+    {
+        // Get the home directory and add our directory name.
+        const char *envVal = getenv("HOME");
+        char* resolved_path = realpath(envVal, nullptr);
+        if( resolved_path )
+        {
+            directoryName = resolved_path;
+            free(resolved_path);
+            resolved_path = nullptr;
+        }
+        else
+        {
+#ifdef __ANDROID__
+            directoryName = "/sdcard/Intel";
+#else
+            directoryName = "/tmp/Intel";
+#endif
+        }
+        directoryName += "/";
+        directoryName += subDir;
+        directoryName += "/";
+
+        // Add the process name to the directory name.
+        directoryName += GetProcessName();
+    }
+
+#ifdef __ANDROID__
+    __android_log_print(ANDROID_LOG_INFO, "clIntercept", "dumpDir=%s\n", directoryName.c_str());
+#endif
+}
+
+inline void Services::GetDumpDirectoryName(
+    const std::string& subDir,
+    std::string& directoryName ) const
+{
+    GetDumpDirectoryNameWithoutPid(subDir, directoryName);
+    if( APPEND_PID )
+    {
+        directoryName += ".";
+        directoryName += std::to_string(GetProcessID());
+    }
+}
+
+inline void Services::MakeDumpDirectories(
+    const std::string& fileName ) const
+{
+    // The first directory name is the root.  We don't
+    // have to make a directory for it.
+    std::string::size_type  pos = fileName.find( "/" );
+
+    pos = fileName.find( "/", ++pos );
+    while( pos != std::string::npos )
+    {
+        mkdir(
+            fileName.substr( 0, pos ).c_str(),
+            0777 );
+
+        pos = fileName.find( "/", ++pos );
+    }
 }
 
 inline bool Services::GetCLInterceptName(
--- intercept/OS/OS_mac.cpp
+++ intercept/OS/OS_mac.cpp
@@ -9,12 +9,134 @@
 namespace OS
 {
 
-Services::Services( void* poGlobalData )
+const char* Services::ENV_PREFIX = "";
+const char* Services::CONFIG_FILE = "config.conf";
+const char* Services::SYSTEM_DIR = "/etc";
+const char* Services::LOG_DIR = NULL;
+bool Services::APPEND_PID = false;
+
+Services::Services( void* poGlobalData ) {}
+
+bool Services::GetControl(
+    const std::string& name,
+    void* pValue,
+    size_t size ) const
 {
+    // Look at environment variables first:
+    {
+        std::string envName(ENV_PREFIX);
+        envName += name;
+        const char *envVal = getenv(envName.c_str());
+        if( ( envVal != NULL ) && ( size == sizeof(unsigned int) ) )
+        {
+            unsigned int *puVal = (unsigned int *)pValue;
+            *puVal = atoi(envVal);
+            return true;
+        }
+        else if( ( envVal != NULL ) && ( strlen(envVal) < size ) )
+        {
+            char* pStr = (char*)pValue;
+            strcpy( pStr, envVal );
+            return true;
+        }
+    }
+
+    // Look at config files second:
+    bool    found = false;
+    std::string fileName;
+
+    // First, check for a config file in the HOME directory.
+    const char *envVal = getenv("HOME");
+    if( !found && envVal != NULL )
+    {
+        fileName = envVal;
+        fileName += "/";
+        fileName += CONFIG_FILE;
+        found = GetControlFromFile(
+            fileName,
+            name,
+            pValue,
+            size );
+    }
+
+    // Finally, check the "system" directory.
+    if( !found )
+    {
+        fileName = SYSTEM_DIR;
+        fileName += "/";
+        fileName += CONFIG_FILE;
+        found = GetControlFromFile(
+            fileName,
+            name,
+            pValue,
+            size );
+    }
+
+    return found;
 }
 
-Services::~Services()
+bool Services::GetControlFromFile(
+    const std::string& fileName,
+    const std::string& controlName,
+    void* pValue,
+    size_t size ) const
 {
+    bool    found = false;
+
+    std::ifstream   is;
+    std::string     s;
+
+    is.open( fileName.c_str() );
+    if( is.fail() )
+    {
+        return false;
+    }
+
+    while( !is.eof() && !found )
+    {
+        std::getline(is, s);
+
+        // skip blank lines
+        if( s.length() == 0 )
+        {
+            continue;
+        }
+        // skip "comment" lines
+        if( s.find(";") == 0 || s.find("#") == 0 || s.find("//") == 0 )
+        {
+            continue;
+        }
+
+        size_t  pos = s.find('=');
+        if( pos != std::string::npos )
+        {
+            std::string var = s.substr( 0, pos );
+            var.erase(std::remove_if(var.begin(), var.end(), ::isspace), var.end());
+
+            std::string value = s.substr( pos + 1 );
+            value.erase(std::remove_if(value.begin(), value.end(), ::isspace), value.end());
+
+            if( var == controlName )
+            {
+                if( size == sizeof(unsigned int) )
+                {
+                    unsigned int* pUIValue = (unsigned int*)pValue;
+                    std::istringstream iss(value);
+                    iss >> pUIValue[0];
+                    found = true;
+                }
+                else if( value.length() < size )
+                {
+                    char* pStr = (char*)pValue;
+                    strcpy( pStr, value.c_str() );
+                    found = true;
+                }
+            }
+        }
+    }
+
+    is.close();
+    return found;
 }
 
 }
